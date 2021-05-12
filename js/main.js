cb2a187524a8b05a516fb9d1a099dea0f25f7a17
KovacAca <kovacaleksandar1@gmail.com>
2021-05-13 00:51:07 +0200

    welcome

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.element', {
+  // Waits 1000ms after typing "First"
+  strings: ['Arabic', 'Chinese', 'Croatian', 'Dutch', 'Finland', 'France', 'English', 'Italia'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  smartBackspace: true,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: false,
+});
